c8fe7a137f3f847a07c4206e91cf711c40170bf3
valneja stojčič <valneja stoji>
2017-01-13 22:34:37 +0100

    fix

--- server.js
+++ server.js
@@ -45,38 +45,33 @@ app.use(
 );
 
 //__________________________________________________________
+//for goals
 var sporocilo = "";
+
+//logging
 var winston = require('winston');
 var logger = new (winston.Logger)({
     transports: [
-      new (winston.transports.Console)(),
-      new (winston.transports.File)({ filename: 'info.log' })
+	    new (winston.transports.Console)(),
+	    new (winston.transports.File)({ filename: 'info.log' })
     ]
-  });
+});
 
+// log in 
 app.post('/prijava', function(zahteva, odgovor) {
-	//zahteva.send('hi');
+	
 	var form = new formidable.IncomingForm();
 	form.parse(zahteva, function(error, podatki){
-		//console.log(podatki.userName);
-		//console.log(podatki.password);
-
 		client.query("SELECT * FROM expenses.users WHERE username = $1", [podatki.userName], function(err, rezultat){
-				//console.log(rezultat);
 			if(rezultat.rows.length != 0 && bcrypt.compareSync(podatki.password, rezultat.rows[0].password)){
-			
-			
-				//console.log("prijava id "+ rezultat.rows[0].user_id);
-				
+				//session
 				zahteva.session.id1 = rezultat.rows[0].user_id;
 				zahteva.session.role = rezultat.rows[0].pro;
-				//console.log("pro: "+zahteva.session.role);
+				
     			odgovor.redirect('/lifeExpenses');
 			}
 			else{
 				logger.info('Wrong username or password!');
-
-				//console.log("Wrong username or password!");
 				odgovor.redirect('/');
 			}
 			
@@ -84,16 +79,16 @@ app.post('/prijava', function(zahteva, odgovor) {
 		
 	});
 });
+
+//register 
 app.get('/register',function(zahteva,odgovor){
 	odgovor.render('register');
-
 })
 
 app.post('/registracija', function(zahteva, odgovor){
 	var form = new formidable.IncomingForm();
 	var obstaja = 0;
 	form.parse(zahteva, function(error, podatki){
-
 		var userName= podatki.userName;
 		if(podatki.pass2 == podatki.pass){	
 			var cryptedPass = bcrypt.hashSync(podatki.pass);	
@@ -104,28 +99,19 @@ app.post('/registracija', function(zahteva, odgovor){
 						client.query("INSERT INTO expenses.account (user_id, name, credit, id) VALUES ($1,$2,$3,$4)", [r.rows[0].user_id, "savings", 0, 2], function(e2,r2){
 							client.query("INSERT INTO expenses.account (user_id, name, credit, id) VALUES ($1,$2,$3,$4)", [r.rows[0].user_id, "wallet", 0, 3], function(e3,r3){
 								if(zahteva){
-									//console.log("User already exists");
 									logger.info('User already exists');
 									odgovor.redirect('/register');
 								}
 								if(e1||e2||e3){
-									//console.log("error "+e1+" "+e2+" "+e3);
 									logger.error("error "+e1+" "+e2+" "+e3);
 								}
 								else{
-									//console.log("Uspesna registracija!");
-									
 									odgovor.redirect('/');
 								}
 							});
 						});
 					});
 				})
-				
-			
-					//console.log("zahteva: "+zahteva);
-					//console.log("odgovor: "+odgovor);
-					
 			});
 		}
 		else{
@@ -135,38 +121,39 @@ app.post('/registracija', function(zahteva, odgovor){
 	});
 })
 
+//adds goal
 app.post('/goal', function(zahteva,odgovor){
 	var form = new formidable.IncomingForm();
 	form.parse(zahteva, function(error, podatki){
 		client.query("SELECT * FROM expenses.goal WHERE user_id = $1", [zahteva.session.id1], function(e, cilji){
+			//just a Pro user can add more than 3 goals
 			if(cilji.rows.length < 3){
 				client.query("INSERT INTO expenses.goal (amount, date, user_id) VALUES ($1,$2,$3)", [podatki.value, podatki.date, zahteva.session.id1], function(err, podatki){
-						if(err){
-							//console.log("napaka: "+ err);
-							logger.error("napaka: "+ err);
-						}
-						else{
-							//console.log("odgovor: "+odgovor);
+					if(err){
 						
-							odgovor.redirect('/lifeExpenses');
-						}				
+						logger.error("napaka: "+ err);
+					}
+					else{
+						
+					
+						odgovor.redirect('/lifeExpenses');
+					}				
 				});
 			}	
 			else if(zahteva.session.role == true){
 				client.query("INSERT INTO expenses.goal (amount, date, user_id) VALUES ($1,$2,$3)", [podatki.value, podatki.date, zahteva.session.id1], function(err, podatki){
-						if(err){
-							//console.log("napaka: "+ err);
-							logger.error("napaka: "+ err);
-						}
-						else{
-							//console.log("odgovor: "+odgovor);
+					if(err){
 						
-							odgovor.redirect('/lifeExpenses');
-						}				
+						logger.error("napaka: "+ err);
+					}
+					else{
+						
+					
+						odgovor.redirect('/lifeExpenses');
+					}				
 				});
 			}
 			else{
-				
 				sporocilo = "Upgrade to Pro";
 				odgovor.redirect('/lifeExpenses');
 			}
@@ -174,41 +161,36 @@ app.post('/goal', function(zahteva,odgovor){
 	});
 });
 
+//delete choosen goal
 app.post('/deleteGoal', function(zahteva, odgovor){
 	var form = new formidable.IncomingForm();
 	form.parse(zahteva, function(error, podatki){
-
 		client.query("DELETE FROM expenses.goal WHERE goal_id = $1", [podatki.id], function(err, podatki){
-					if(err){
-						logger.error("napaka: "+ err);
-						//console.log("napaka: "+ err);
-					}
-					else{
-						//console.log("odgovor: "+odgovor);
-						
-					
-						odgovor.redirect('/lifeExpenses');
-					}
-					
-					
-			});
+			if(err){
+				logger.error("napaka: "+ err);
+				
+			}
+			else{
+				
+				odgovor.redirect('/lifeExpenses');
+			}	
+		});
 	});
 });
 
+//log out
 app.get('/odjava', function(zahteva, odgovor){
 
 	zahteva.session.id1 = null;
 	zahteva.session.role = null;
-	//console.log("odjava");
+	logger.info("Log out. User id: "+zahteva.session.id1)
 	odgovor.redirect('/');
 })
 
+//main page
 app.get('/lifeExpenses', function(zahteva,odgovor){
 
-	//console.log("prijavljen z id "+ zahteva.session.id);
-	//console.log("prijavljen z id1 "+ zahteva.session.id1);
 	if(zahteva.session.id1 == null){
-		//console.log("ni uporabnika");
 		logger.info("You need to log in!");
 		odgovor.redirect('/');
 	}
@@ -219,7 +201,7 @@ app.get('/lifeExpenses', function(zahteva,odgovor){
 			client.query("SELECT * FROM expenses.account WHERE user_id = $1 ORDER BY id ASC", [zahteva.session.id1], function(err, racuni){
 				client.query("SELECT value FROM expenses.action WHERE account_id = 1 AND user_id = $1 AND effect = -1 ORDER by action_id DESC LIMIT 12", [zahteva.session.id1], function(e, p){
 					client.query("SELECT value FROM expenses.action WHERE account_id = 1 AND user_id = $1 AND effect = 1 ORDER by action_id DESC LIMIT 12", [zahteva.session.id1], function(e1, p1){
-						//console.log(p.rows.length);
+						//arrays for graph
 						var tab = [0,0,0,0,0,0,0,0,0,0,0,0];
 						var tab2 = [0,0,0,0,0,0,0,0,0,0,0,0];
 						for(var i = 0; i < p.rows.length; i++){
@@ -230,40 +212,27 @@ app.get('/lifeExpenses', function(zahteva,odgovor){
 							tab2[i] = p1.rows[i].value;
 						}
 
-						//console.log(tab);
 						odgovor.render('lifeExpenses', {cilji: cilji.rows, racuni: racuni.rows, tab1: tab, tab2: tab2, sporocilo: sporocilo});
 						sporocilo = "";
 					});
 				});	
 			});
-			
-			//console.log(cilji.rows);
-			
-			//odgovor.render('lifeExpenses', {cilji: rezultat.rows});	
 		});
 	}
 });
 
 app.post('/transaction', function(zahteva,odgovor){
-	/*client.query("SELECT * FROM expenses.account WHERE account_id = 1", function(err, podatki){
-		odgovor.render('transactionAcc', {credit: podatki.rows[0].credit});
-	});*/
 	odgovor.redirect("/transactionAcc");
 });
 
 app.post('/savings', function(zahteva,odgovor){
-	/*client.query("SELECT * FROM expenses.account WHERE account_id = 2", function(err, podatki){
-		odgovor.render('savingsAcc', {credit: podatki.rows[0].credit});
-	});*/
 	odgovor.redirect("/savingsAcc");
 });
 app.post('/wallet', function(zahteva,odgovor){
-	/*client.query("SELECT * FROM expenses.account WHERE account_id = 3", function(err, podatki){
-		odgovor.render('wallet', {credit: podatki.rows[0].credit});
-	});*/
 	odgovor.redirect("/wallet");
 });
 
+//accounts
 app.get('/transactionAcc', function(zahteva,odgovor){
 	client.query("SELECT * FROM expenses.account WHERE id = 1 AND user_id = $1", [zahteva.session.id1], function(err, podatki){
 		client.query("SELECT * FROM expenses.action WHERE account_id = 1 AND user_id = $1 ORDER by action_id DESC LIMIT 10", [zahteva.session.id1], function(e, p){
@@ -289,7 +258,7 @@ app.get('/wallet', function(zahteva,odgovor){
 	});
 });
 
-
+//transfer money from one account to another
 app.post('/transfer', function(zahteva,odgovor){
 	var form = new formidable.IncomingForm();
 	form.parse(zahteva, function(error, podatki){
@@ -315,24 +284,19 @@ app.post('/transfer', function(zahteva,odgovor){
 				client.query("INSERT INTO expenses.action (name,type,date,effect,value,account_id, user_id) VALUES ($1,$2,$3,$4,$5,$6,$7)", [imeFrom, "transaction", d, 1, vrednost, idTo, zahteva.session.id1], function(e,r){
 					client.query("INSERT INTO expenses.action (name,type,date,effect,value,account_id, user_id) VALUES ($1,$2,$3,$4,$5,$6,$7)", [imeTo, "transaction", d, -1, vrednost, idFrom, zahteva.session.id1], function(e1,r1){
 						if(err ){
-							//console.log("Napaka pri transakciji "+err);
 							logger.error("Napaka pri transakciji "+err);
 						}
 						if(e){
 							logger.error("Napaka pri transakciji "+e);
-							//console.log("Napaka pri transakciji "+e);
 						}
 						if(e1){
 							logger.error("Napaka pri transakciji "+e1);
-							//console.log("Napaka pri transakciji "+e1);
 						}
 						if(err1){
 							logger.error("Napaka pri transakciji "+err1);
-							//console.log("Napaka pri transakciji "+err1);
 						}
 						else{
 							logger.info("Uspesno");
-							//console.log("Uspesno");
 						}	
 						
 						if(idFrom == 1){
@@ -350,40 +314,27 @@ app.post('/transfer', function(zahteva,odgovor){
 			});
 		});
 	});
-
-
 });
 
+
+//add action to choosen account
 app.post('/addAction', function(zahteva, odgovor){
 	var form = new formidable.IncomingForm();
     
-    //console.log("input: "+zahteva.body);
-    
 	form.parse(zahteva, function(error, podatki){
-		//console.log("input: "+podatki.radioAction);
-		//console.log("amount "+podatki.amount);
 		var vrednost = podatki.amount;
 		var id = podatki.accid;
 		var income = -1;
 		var type = podatki.tip;
 		if(podatki.radioAction == "income")
 			income=1;
-		//console.log("income= "+ income);
-		//console.log("type: "+podatki.tip);
 		client.query("INSERT INTO expenses.action (name,type,date,effect,value,account_id,user_id) VALUES ($1,$2,$3,$4,$5,$6,$7)", [podatki.name, type, podatki.date, income, podatki.amount, podatki.accid, zahteva.session.id1], function(err, podatki){
-			//console.log("Poteka transakcija");
-
-			if(income == 1 && err == null){
-				//console.log("pristej amount "+vrednost);
+			
+			if(income == 1 && err == null){ //income
 				client.query("UPDATE expenses.account SET credit = credit+$1 WHERE account_id = $2", [vrednost, id], function(napaka, vsebina){
 					if (napaka) {
 						logger.error("Napaka pri posodabljanju stanja na racunu: "+ napaka);
-						//console.log("Napaka pri posodabljanju stanja na racunu: "+ napaka);
 					}
-					/*console.log("vsebina: "+ vsebina);
-					console.log("Posodobljen racun");
-					console.log("odgovor akcije: "+ podatki);
-					console.log("err akcije: "+ err);*/
 					switch (id){
 						case 1:
 							odgovor.redirect('/transactionAcc');
@@ -395,55 +346,36 @@ app.post('/addAction', function(zahteva, odgovor){
 							odgovor.redirect('/wallet');
 							break;
 					}
-					
-			
 				});
 			}
-			else if(income == -1 && err == null){
-				
-				//console.log("odstej amount "+vrednost);
-				//
+			else if(income == -1 && err == null){ //outcome
 				client.query("UPDATE expenses.account SET credit = credit-$1 WHERE account_id = $2", [vrednost, id], function(napaka, vsebina){
 					if (napaka) {
 						console.log("Napaka pri posodabljanju stanja na racunu: "+ napaka);
 						logger.error("Napaka pri posodabljanju stanja na racunu: "+ napaka);
 					}
-					/*console.log("vsebina: "+ vsebina);
-					console.log("Posodobljen racun");
-					console.log("odgovor akcije: "+ podatki);
-					console.log("err akcije: "+ err);*/
-					//console.log("id "+id);
 					if(id == 1){
-						//console.log("1");
 						odgovor.redirect('/transactionAcc');
 					}
 						
 						
 					if(id == 2){
-						//console.log("2");
 						odgovor.redirect('/savingsAcc');
 					}
 						
 					if(id == 3){
-						//console.log("3");
 						odgovor.redirect('/wallet');
-					}
-						
-					
-			
+					}	
 				});
-			}
-				
+			}	
 		});
 	});
 });
 
 
 
-
+//log in
 app.get('/', function(zahteva, odgovor) {
-	
-	//console.log("session id "+ zahteva.session.id1);
     odgovor.render('index');
 });
 
@@ -453,13 +385,3 @@ http.listen(process.env.PORT, function() {
   logger.info("Strežnik posluša na portu " + process.env.PORT + ".");
 });
 
-
-//var query = client.query("INSERT INTO expenses.account(account_id, credit) values(6,'2.20')");
-/*var query = client.query("SELECT * FROM expenses.users WHERE user_id=3", function(e,r){
-	if(r.rows.length !=0){
-		console.log(r.rows);	
-	}
-		
-});
-
-*/
